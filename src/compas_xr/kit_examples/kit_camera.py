a9c2f2774981d7ff64f8ca8a9c5347286666b0d7
Romana Rust <rust@arch.ethz.ch>
2021-01-11 16:53:30 +0100

    Update kit_camera.py

--- src/compas_xr/kit_examples/kit_camera.py
+++ src/compas_xr/kit_examples/kit_camera.py
@@ -1,24 +1,28 @@
 import sys
-sys.path.append('C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages')
-import roslibpy
+
+paths = ['C:\\Users\\rustr\\.conda\\envs\\xr\\DLLs', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib', 'C:\\Users\\rustr\\.conda\\envs\\xr', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\matplotlib-3.0.3-py3.6-win-amd64.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\imageio-2.9.0-py3.6.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\watchdog-0.10.4-py3.6.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\sympy-1.7rc1-py3.6.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\scipy-1.5.4-py3.6-win-amd64.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\jsonschema-3.2.0-py3.6.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\schema-0.7.3-py3.6.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\pycollada-0.7.1-py3.6.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\planarity-0.4.1-py3.6-win-amd64.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\numpy-1.19.3-py3.6-win-amd64.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\numba-0.52.0rc3-py3.6-win-amd64.egg', 'C:\\Users\\rustr\\.conda\\envs\\xr\\lib\\site-packages\\networkx-2.5-py3.6.egg', 'c:\\users\\rustr\\workspace\\compas_xr\\src', 'c:\\users\\rustr\\workspace\\libraries\\pyopenvr\\src']
+for path in paths:
+    sys.path.append(path)
+
+from compas_fab.backends import RosClient
+from compas_fab.backends.ros.messages import PoseStamped
 from roslibpy import Topic
 from roslibpy import Ros
 import omni.usd
 import asyncio
 import time
 
+url = "omniverse://localhost/Users/rr/semiramis/semiramis.project.usd"
+
+
 def topic_callback(msg):
     print(msg)
+    frame = PoseStamped.from_msg(msg).pose.frame
+    print(frame)
+
 
 async def task():
-    client = Ros(host='localhost', port=9090)
-    client.run()
-    
-    topic = Topic(client, '/camera_position', 'geometry_msgs/PoseStamped')
-    topic.subscribe(topic_callback)
-    
     omni.client.usd_live_set_default_enabled(True)
-    url = "omniverse://localhost/Users/rr/semiramis/semiramis.project.usd"
     await omni.usd.get_context().open_stage_async(url)
     context = omni.usd.get_context()
     stage = context.get_stage()
@@ -31,10 +35,12 @@ async def task():
     print(attr)
     print(attr.Get())
     await omni.usd.get_context().save_as_stage_async(url)
+
+with RosClient() as client:
+    topic = Topic(client, '/camera_position', 'geometry_msgs/PoseStamped')
+    topic.subscribe(topic_callback)
+    asyncio.ensure_future(task())
     time.sleep(1)
-    client.terminate()
-    
-asyncio.ensure_future(task())
 
 
 
