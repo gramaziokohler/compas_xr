12b32f086cfc3f8571df70d9f9330ff9fb90a169
Romana Rust <rust@arch.ethz.ch>
2021-02-23 18:57:06 +0100

    Update controller_input_to_ros.py

--- src/compas_xr/openvr_examples/controller_input_to_ros.py
+++ src/compas_xr/openvr_examples/controller_input_to_ros.py
@@ -1,11 +1,59 @@
+"""
+k_EButton_A = 7
+k_EButton_ApplicationMenu = 1
+k_EButton_Axis0 = 32
+k_EButton_Axis1 = 33
+k_EButton_Axis2 = 34
+k_EButton_Axis3 = 35
+k_EButton_Axis4 = 36
+k_EButton_DPad_Down = 6
+k_EButton_DPad_Left = 3
+k_EButton_DPad_Right = 5
+k_EButton_DPad_Up = 4
+k_EButton_Dashboard_Back = 2
+k_EButton_Grip = 2
+k_EButton_IndexController_A = 2
+k_EButton_IndexController_B = 1
+k_EButton_IndexController_JoyStick = 35
+k_EButton_Max = 64
+k_EButton_ProximitySensor = 31
+k_EButton_SteamVR_Touchpad = 32
+k_EButton_SteamVR_Trigger = 33
+k_EButton_System = 0
+
+
+openvr.VREvent_ButtonTouch
+openvr.VREvent_ButtonUntouch
+openvr.VREvent_ButtonPress
+openvr.VREvent_ButtonUnpress
+
+# https://gist.github.com/awesomebytes/75daab3adb62b331f21ecf3a03b3ab46
+
+# pose = poses[i]
+        # pose_info = dict(
+        #    model_name=model_name,
+        #    device_is_connected=pose.bDeviceIsConnected,
+        #    device_is_controller=is_controller,
+        #    valid=pose.bPoseIsValid,
+        #    tracking_result=pose.eTrackingResult,
+        #    d2a=pose.mDeviceToAbsoluteTracking,
+        #    velocity=pose.vVelocity,                   # m/s
+        #    angular_velocity=pose.vAngularVelocity     # radians/s?
+        # )
+"""
+
 import openvr
 
 from roslibpy import Topic
 from roslibpy import Ros
-from roslibpy import Message
 
-from compas_fab.backends.ros.messages import Pose, PoseStamped
-from compas.geometry import Transformation, Frame
+from compas_fab.backends.ros.messages import Pose
+from compas_fab.backends.ros.messages import PoseStamped
+from compas.geometry import Transformation
+from compas.geometry import Frame
+
+from compas_xr.messages import Int8MultiArray
+from compas_xr.messages import Float32MultiArray
 
 # ros
 client = Ros(host='localhost', port=9090)
@@ -13,18 +61,23 @@ client.run()
 
 model_names = ['generic_hmd', 'controller_left', 'controller_right', 'OptiTrackRigidBody', 'lh_basestation_valve_gen2']
 position_talkers = dict(zip(model_names, [Topic(client, '/position_%s' % name, 'geometry_msgs/PoseStamped') for name in model_names]))
-button_talkers = dict(zip(model_names, [Topic(client, '/button_%s' % name, 'std_msgs/String') for name in model_names]))
+hands = ["left", "right"]
+hands_idx = dict(zip(hands, [None for _ in range(len(hands))]))
 
-for k, topic in position_talkers.items():
-    topic.advertise()
-for k, topic in button_talkers.items():
-    topic.advertise()
+buttonpress_talker = dict(zip(hands, [Topic(client, '/buttonpress_%s' % hand, 'std_msgs/Int8MultiArray') for hand in hands]))
+trackpad_talker = dict(zip(hands, [Topic(client, '/trackpad_%s' % hand, 'std_msgs/Float32MultiArray') for hand in hands]))
+
+button_idx = [openvr.k_EButton_SteamVR_Trigger, openvr.k_EButton_SteamVR_Touchpad, openvr.k_EButton_Grip, openvr.k_EButton_A, openvr.k_EButton_IndexController_B]
+button_values = [0 for _ in range(len(button_idx))]
+
+for d in [position_talkers, buttonpress_talker, trackpad_talker]:
+    for k, topic in d.items():
+        topic.advertise()
 
 openvr.init(openvr.VRApplication_Scene)
 
+
 def get_model_name(vrsys, dix, device_class):
-    """
-    """
     if device_class == openvr.TrackedDeviceClass_Controller:
         if vrsys.getControllerRoleForTrackedDeviceIndex(dix) == openvr.TrackedControllerRole_RightHand:
             model_name = 'controller_right'
@@ -48,53 +101,61 @@ while client.is_connected:
         is_controller = device_class == openvr.TrackedDeviceClass_Controller
         is_beacon = device_class == openvr.TrackedDeviceClass_TrackingReference
 
+        # only once needed
+        if device_class == openvr.TrackedDeviceClass_Controller:
+            if vrsys.getControllerRoleForTrackedDeviceIndex(i) == openvr.TrackedControllerRole_RightHand:
+                hands_idx["right"] = i
+            else:
+                hands_idx["left"] = i
+
         model_name = get_model_name(vrsys, i, device_class)
 
-        """
-        pose = poses[i]
-        pose_info = dict(
-            model_name=model_name,
-            device_is_connected=pose.bDeviceIsConnected,
-            device_is_controller=is_controller,
-            valid=pose.bPoseIsValid,
-            tracking_result=pose.eTrackingResult,
-            d2a=pose.mDeviceToAbsoluteTracking,
-            velocity=pose.vVelocity,                   # m/s
-            angular_velocity=pose.vAngularVelocity     # radians/s?
-        )
-        """
         T = Transformation(list(poses[i].mDeviceToAbsoluteTracking) + [[0, 0, 0, 1]])
         pose_msg = Pose.from_frame(Frame.from_transformation(T))
         pose_stamped_msg = PoseStamped(pose=pose_msg)
         position_talkers[model_name].publish(pose_stamped_msg.msg)
-    
-    new_event = openvr.VREvent_t()
+
+    hands_idx_inv = {v: k for k, v in hands_idx.items()}
     vrsys = openvr.VRSystem()
+
+    # controllers
+    for hand, dix in hands_idx.items():
+        result, pControllerState = vrsys.getControllerState(dix)
+
+        trackpad_x = pControllerState.rAxis[0].x
+        trackpad_y = pControllerState.rAxis[0].y
+        trackpad_touched = bool(pControllerState.ulButtonTouched >> 32 & 1)
+        if trackpad_touched:
+            print(trackpad_x, trackpad_y)
+            msg = Float32MultiArray(data=[trackpad_x, trackpad_y])
+            trackpad_talker[hand].publish(msg.msg)
+
+    # button presses
+
+    new_event = openvr.VREvent_t()
 
     while vrsys.pollNextEvent(new_event):
 
         dix = new_event.trackedDeviceIndex
         device_class = vrsys.getTrackedDeviceClass(dix)
-        # We only want to watch controller events
-        if device_class != openvr.TrackedDeviceClass_Controller:
-            continue
-        model_name = get_model_name(vrsys, dix, device_class)
-        bix = new_event.data.controller.button
-        # Pay attention to trigger presses only
-        if bix != openvr.k_EButton_SteamVR_Trigger:
-            print("bix", bix)
-            continue
-        device_index = dix
-        t = new_event.eventType
-        actions = {openvr.VREvent_ButtonTouch : "touch",
-                   openvr.VREvent_ButtonUntouch : "untouch",
-                   openvr.VREvent_ButtonPress : "press",
-                   openvr.VREvent_ButtonUnpress : "unpress",}        
-        button_talkers[model_name].publish(Message({'data': actions[t]}))
+        if device_class == openvr.TrackedDeviceClass_Controller:
+            hand = hands_idx_inv[dix]
+            bix = new_event.data.controller.button
+            if bix in button_idx:
+                i = button_idx.index(bix)
+                if new_event.eventType == openvr.VREvent_ButtonPress:
+                    button_values[i] = 1
+                if new_event.eventType == openvr.VREvent_ButtonUnpress:
+                    button_values[i] = 0
+                print(button_values)
+                # button_state_talker.publish()
+                msg = Int8MultiArray(data=button_values)
+                buttonpress_talker[hand].publish(msg.msg)
 
-for name in model_names:
-    position_talkers[name].unadvertise()
-    button_talkers[name].unadvertise()
+
+for d in [position_talkers, buttonpress_talker, trackpad_talker]:
+    for k, topic in d.items():
+        topic.unadvertise()
 
 client.terminate()
 
