9a3f66f77e58ce3d1ea7ebd3479137ad57cfc0b9
Romana Rust <rust@arch.ethz.ch>
2021-03-24 08:29:28 +0100

    Update client.py

--- src/compas_xr/omniverse/client.py
+++ src/compas_xr/omniverse/client.py
@@ -12,6 +12,42 @@ def open_stage(url):
     return Usd.Stage.Open(url)
 
 
+async def open_stage_live(url):
+    """Opens the stage.
+    """
+    omni.client.usd_live_set_default_enabled(True)
+    context = omni.usd.get_context()
+    await context.open_stage_async(url)
+    context.set_stage_live(omni.usd.StageLiveModeType.ALWAYS_ON)
+    return context.get_stage()
+
+
+def start_omniverse():
+    """Startup Omniverse."""
+    """
+    # Register a function to be called whenever the library wants to print something to a log
+    omni.client.set_log_callback(logCallback)
+
+    # The default log level is "Info", set it to "Debug" to see all messages
+    omni.client.set_log_level(eOmniClientLogLevel_Debug)
+
+    # Initialize the library and pass it the version constant defined in OmniClient.h
+    # This allows the library to verify it was built with a compatible version. It will
+    # return false if there is a version mismatch.
+    if not omni.client.initialize(kOmniClientVersion):
+        return EXIT_FAILURE
+    omni.client.register_connection_status_callback(OmniClientConnectionStatusCallbackImpl)
+    """
+    omni.client.usd_live_set_default_enabled(True) # Enable live updates
+    return True
+
+
+def shutdown_omniverse():
+    """Shut down Omniverse connection."""
+    omni.client.usd_live_wait_for_pending_updates()
+    omni.client.shutdown()
+
+
 def remove_mesh(stage_url, prim_path):
     stage = open_stage(stage_url)
     stage.RemovePrim(prim_path)
