b721d8cdefee3c8c6fa7de3831477c10af2435fb
Romana Rust <rust@arch.ethz.ch>
2021-03-24 08:29:37 +0100

    Update conversion.py

--- src/compas_xr/pixar/conversion.py
+++ src/compas_xr/pixar/conversion.py
@@ -2,6 +2,7 @@ import math
 
 from pxr import Gf
 
+from compas.geometry import Frame
 from compas.geometry import Rotation
 from compas.geometry import transpose_matrix
 
@@ -13,3 +14,8 @@ def gfmatrix4d_from_transformation(transformation):
 def translate_and_rotateZYX_from_frame(frame):
     rotzyx = [math.degrees(a) for a in Rotation.from_frame(frame).euler_angles(False, 'xyz')]
     return Gf.Vec3f(*frame.point), Gf.Vec3f(*rotzyx)
+
+
+def frame_from_translate_and_rotateZYX(translate, rotateZYX):
+    rotateZYX_radians = [math.radians(a) for a in list(rotateZYX)]
+    return Frame.from_euler_angles(rotateZYX_radians, static=False, axes='xyz', point=translate)
