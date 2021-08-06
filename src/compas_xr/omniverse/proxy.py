1bcceedd6d456d13acf67c2f5b67ba4b901da23d
Romana Rust <rust@arch.ethz.ch>
2021-08-06 08:26:43 +0200

    autopep8

--- src/compas_xr/omniverse/proxy.py
+++ src/compas_xr/omniverse/proxy.py
@@ -1,11 +1,12 @@
+from compas.rpc.services.default import start_service
 import sys
 sys.path.append(r"C:\Users\3duser\.conda\envs\xr\Lib\site-packages")
 sys.path.append(r"C:\Users\3duser\workspace\compas_xr\src")
 
-from compas.rpc.services.default import start_service
 
 def start(port, autoreload, **kwargs):
     start_service(port, autoreload)
+
 
 if __name__ == '__main__':
     start(port=1753, autoreload=True)
