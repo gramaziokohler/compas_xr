6e97f0b467ff73a16d583f84664dcdcc9480f928
Romana Rust <rust@arch.ethz.ch>
2022-07-25 21:27:10 +0200

    Update __init__.py

--- src/compas_xr/files/usd/__init__.py
+++ src/compas_xr/files/usd/__init__.py
@@ -1,6 +1,3 @@
-import compas
-
-if not compas.IPY:
-    from .usd import *  # noqa F403
-    from .material import *  # noqa F403
-    from .scene import USDScene
+from .usd import *  # noqa F403
+from .material import *  # noqa F403
+from .scene import USDScene  # noqa F403
