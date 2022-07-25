c8ab2dd17309e77b93c760a7c7ff4a348b0674ca
Romana Rust <rust@arch.ethz.ch>
2022-07-25 22:27:30 +0200

    Update usd.py

--- src/compas_xr/files/usd/usd.py
+++ src/compas_xr/files/usd/usd.py
@@ -1,131 +1,28 @@
-
+"""
+TODO: This file should be removed and contents migrated to compas_usd
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
-import math
+
+from pxr import UsdGeom
+from compas_nurbs.utilities import unflatten
 from compas.utilities import flatten
-
 from compas.geometry import transpose_matrix
-from compas.geometry import Frame
-from compas.geometry import Scale
 from compas.geometry import Rotation
-from compas.geometry import Transformation
-
-
-def apply_frame_transformation_on_prim(prim, frame):
-    """
-    """
-    from pxr import UsdGeom
-    xform = UsdGeom.Xformable(prim)
-    transform = xform.AddTransformOp()
-    matrix = gfmatrix4d_from_transformation(Transformation.from_frame(frame))
-    transform.Set(matrix)
-
-
-def apply_transformation_on_prim(prim, transformation):
-    """
-    """
-    from pxr import UsdGeom
-    xform = UsdGeom.Xformable(prim)
-    transform = xform.AddTransformOp()
-    matrix = gfmatrix4d_from_transformation(transformation)
-    transform.Set(matrix)
-
-
-def apply_rotate_and_translate_on_prim(prim, frame):
-    from pxr import UsdGeom
-    _, _, _, _, rotOrder = UsdGeom.XformCommonAPI(prim).GetXformVectors(0)
-
-    switcher = {UsdGeom.XformCommonAPI.RotationOrderXYZ: "xyz",
-                UsdGeom.XformCommonAPI.RotationOrderXZY: "xzy",
-                UsdGeom.XformCommonAPI.RotationOrderYXZ: "yxz",
-                UsdGeom.XformCommonAPI.RotationOrderYZX: "yzx",
-                UsdGeom.XformCommonAPI.RotationOrderZXY: "zxy",
-                UsdGeom.XformCommonAPI.RotationOrderZYX: "zyx"}
-
-    axes = switcher.get(rotOrder, None)
-    euler_angles = [math.degrees(a) for a in frame.euler_angles(False, axes)]
-    UsdGeom.XformCommonAPI(prim).SetRotate(euler_angles)
-    UsdGeom.XformCommonAPI(prim).SetTranslate(tuple(frame.point))
-
-
-def prim_from_box(stage, path, box):
-    """Returns a `UsdGeom.Cube`
-
-    >>> box = Box(Frame.worldXY(), 1, 1, 1)
-    >>> prim_from_box(stage, "/box", box)
-    UsdGeom.Cube(Usd.Prim(</box>))
-    """
-    from pxr import UsdGeom
-    prim = UsdGeom.Cube.Define(stage, path)
-    prim.GetPrim().GetAttribute('size').Set(1.0)
-    UsdGeom.XformCommonAPI(prim).SetScale((box.xsize, box.ysize, box.zsize))
-    apply_rotate_and_translate_on_prim(prim, box.frame)
-    return prim
-
-
-def prim_from_cylinder(stage, path, cylinder):
-    """Returns a `UsdGeom.Cylinder`
-
-    >>>
-    """
-    from pxr import UsdGeom
-    prim = UsdGeom.Cylinder.Define(stage, path)
-    prim.GetHeightAttr().Set(cylinder.height)
-    prim.GetRadiusAttr().Set(cylinder.radius)
-    prim.GetAxisAttr().Set("Z")
-    # UsdImagingDelegate.SetRefineLevel(path, 2)
-    apply_rotate_and_translate_on_prim(prim, Frame.from_plane(cylinder.plane))
-    return prim
-
-
-def prim_from_sphere(stage, path, sphere):
-    """Returns a ``pxr.UsdGeom.Sphere``
-
-    >>> sphere = Sphere((0, 0, 0), 5)
-    >>> prim_from_sphere(stage, "/sphere", sphere)
-    UsdGeom.Sphere(Usd.Prim(</sphere>))
-    """
-    from pxr import UsdGeom
-    prim = UsdGeom.Sphere.Define(stage, path)
-    prim.GetPrim().GetAttribute('radius').Set(sphere.radius)
-    UsdGeom.XformCommonAPI(prim).SetTranslate(tuple(sphere.point))
-    return prim
-
-
-def prim_from_mesh(stage, path, mesh):
-    """Returns a ``pxr.UsdGeom.Mesh``
-
-    >>> box = Box(Frame.worldXY(), 1, 1, 1)
-    >>> mesh = Mesh.from_shape(box)
-    >>> prim_from_mesh(stage, "/mesh", mesh)
-    UsdGeom.Mesh(Usd.Prim(</mesh>))
-    """
-    from pxr import UsdGeom
-    prim = UsdGeom.Mesh.Define(stage, path)
-    vertices, faces = mesh.to_vertices_and_faces()
-    prim.CreatePointsAttr(vertices)
-    prim.CreateFaceVertexCountsAttr([len(f) for f in faces])
-    prim.CreateFaceVertexIndicesAttr(list(flatten(faces)))
-    return prim
 
 
 def prim_from_surface(stage, path, surface):
     """Returns a ``pxr.UsdGeom.NurbsPatch``
 
-    >>> control_points = [[[0, 0, 0], [0, 4, 0], [0, 8, -3]], [[2, 0, 6], [2, 4, 0], [2, 8, 0]], [[4, 0, 0], [4, 4, 0], [4, 8, 3]], [[6, 0, 0], [6, 4, -3], [6, 8, 0]]]
-    >>> degree = (3, 2)
-    >>> surface = Surface(control_points, degree)
-    >>> prim_from_surface(stage, "/surface", surface)
+    control_points = [[[0, 0, 0], [0, 4, 0], [0, 8, -3]], [[2, 0, 6], [2, 4, 0], [2, 8, 0]], [[4, 0, 0], [4, 4, 0], [4, 8, 3]], [[6, 0, 0], [6, 4, -3], [6, 8, 0]]]
+    degree = (3, 2)
+    surface = Surface(control_points, degree)
+    prim_from_surface(stage, "/surface", surface)
     UsdGeom.NurbsPatch(Usd.Prim(</surface>))
     """
-    from pxr import UsdGeom
-    from compas_nurbs.utilities import unflatten
-    from compas.utilities import flatten
-    from compas.geometry import transpose_matrix
-
     degree_u, degree_v = surface.degree
     knot_vector_u, knot_vector_v = surface.knot_vector
     count_u, count_v = surface.count
@@ -148,20 +45,6 @@ def prim_from_surface(stage, path, surface):
     return prim
 
 
-def prim_default(stage, path, frame=None, scale=None):  # TODO should be transform
-    """
-    """
-    from pxr import UsdGeom
-    prim = UsdGeom.Xform.Define(stage, path)
-    if frame:
-        T = Transformation.from_frame(frame)
-        if scale:
-            print(scale)
-            T = T * Scale.from_factors(scale)
-        apply_transformation_on_prim(prim, T)
-    return prim
-
-
 def reference_filename(stage, reference_name, fullpath=True, extension=None):
     filepath = str(stage.GetRootLayer().resolvedPath)  # .realPath
     if not extension:
@@ -175,71 +58,28 @@ def reference_filename(stage, reference_name, fullpath=True, extension=None):
 
 def prim_instance(stage, path, reference_name, xform=False, extension=None):
     from pxr import UsdGeom
+
     reference_filepath = reference_filename(stage, reference_name, fullpath=False, extension=extension)
     if not xform:
         ref = stage.OverridePrim(path)
-        ref.GetReferences().AddReference('./%s' % reference_filepath)
+        ref.GetReferences().AddReference("./%s" % reference_filepath)
     else:
         ref = UsdGeom.Xform.Define(stage, path)
-        ref.GetPrim().GetReferences().AddReference('./%s' % reference_filepath)
+        ref.GetPrim().GetReferences().AddReference("./%s" % reference_filepath)
 
     ref_xform = UsdGeom.Xformable(ref)
     ref_xform.SetXformOpOrder([])
     return ref
 
 
-def gfmatrix4d_from_transformation(transformation):
-    """
-
-    Examples
-    --------
-    >>> frame = Frame((0, 3, 4), (0.27, 0.95, 0.13), (-0.95, 0.28, -0.09))
-    >>> t1 = Transformation.from_frame(frame)
-    >>> gf = gfmatrix4d_from_transformation(t1)
-    >>> t2 = transformation_from_gfmatrix4d(gf)
-    >>> t1 == t2
-    True
-    """
-    from pxr import Gf
-    return Gf.Matrix4d(*[v for col in transpose_matrix(transformation.matrix) for v in col])
-
-
-def transformation_from_gfmatrix4d(gfmatrix4d):
-    """
-
-    Examples
-    --------
-    >>> frame = Frame((0, 3, 4), (0.27, 0.95, 0.13), (-0.95, 0.28, -0.09))
-    >>> t1 = Transformation.from_frame(frame)
-    >>> gf = gfmatrix4d_from_transformation(t1)
-    >>> t2 = transformation_from_gfmatrix4d(gf)
-    >>> t1 == t2
-    True
-    """
-    matrix = [[v for v in column] for column in gfmatrix4d]
-    return Transformation(transpose_matrix(matrix))
-
-
 def translate_and_orient_from_frame(frame):
     from pxr import Gf
+
     w, x, y, z = Rotation.from_frame(frame).quaternion.wxyz
     return Gf.Vec3f(*frame.point), Gf.Quatd(w, x, y, z)
 
 
-def frame_from_translate_and_rotateZYX(translate, rotateZYX):
-    rotateZYX_radians = [math.radians(a) for a in list(rotateZYX)]
-    return Frame.from_euler_angles(rotateZYX_radians, static=False, axes='xyz', point=translate)
-
-
 if __name__ == "__main__":
+    from compas.geometry import NurbsSurface
 
-    import doctest
-    from pxr import Usd
-    from compas.geometry import Box  # noqa F401
-    from compas.geometry import Sphere  # noqa F401
-    from compas.geometry import Frame  # noqa F401
-    from compas.datastructures import Mesh  # noqa F401
-    from compas_nurbs import Surface  # noqa F401
-
-    stage = Usd.Stage.CreateInMemory()
-    doctest.testmod()
+    s = NurbsSurface()
