91bec2aec0972c875785b3a88ddf70fee7d37f4d
Romana Rust <rust@arch.ethz.ch>
2022-07-25 21:26:35 +0200

    Update scene.py

--- src/compas_xr/datastructures/scene.py
+++ src/compas_xr/datastructures/scene.py
@@ -9,7 +9,7 @@ from compas_xr.files.usd import USDScene
 from compas_xr.files.gltf import GLTFScene
 
 
-__all__ = ["Scene", "Animation"]
+__all__ = ["Scene"]
 
 
 class Scene(Graph):  # or scenegraph
@@ -38,11 +38,11 @@ class Scene(Graph):  # or scenegraph
         self.materials.append(material)
         return len(self.materials) - 1
 
-    # @property
-    # def data(self):
-    #    data_dict = super(Scene, self).data
-    #    data_dict['materials'] = [m.data for m in self.materials]
-    #    return data_dict
+    @property
+    def data(self):
+        data_dict = super(Scene, self).data
+        data_dict["materials"] = [m.data for m in self.materials]
+        return data_dict
 
     @classmethod
     def from_gltf(cls, filepath):
@@ -99,14 +99,63 @@ class Scene(Graph):  # or scenegraph
         return array
 
 
-class Animation(object):
-    pass
-
-
 if __name__ == "__main__":
-    scene = Scene()
-    world = scene.add_node("world")
-    scene.add_node("element", parent=world)
+    import os
+    from compas.geometry import Box
+    from compas.geometry import Frame, Rotation, Vector
+    from compas_xr import DATA
+    from compas_xr.datastructures import Material
+    from compas_xr.datastructures.material import PBRMetallicRoughness
 
-    scene.add_node("element", parent=None)
+    scene = Scene()
+    world = scene.add_layer("world")
+
+    material = Material(name="material")
+    material.pbr_metallic_roughness = PBRMetallicRoughness()
+    material.pbr_metallic_roughness.base_color_factor = [0.9, 0.4, 0.2, 1.0]
+    material.pbr_metallic_roughness.metallic_factor = 0.0
+    material.pbr_metallic_roughness.roughness_factor = 0.5
+    mkey = scene.add_material(material)
+
+    # frame = Frame.worldXY()
+    # frame.point = [1, 2, 3]
+    # frame = Frame.from_euler_angles([0.1, 0.2, 0.3], point=[1, 2, 3])
+
+    rotation = Rotation.from_basis_vectors(Vector(0.936, 0.275, -0.218), Vector(-0.274, 0.961, 0.037))
+    frame = Frame.from_rotation(rotation, point=[1, 2, 3])
+    box = Box(frame, 1, 1, 1)
+    scene.add_layer("box", parent=world, element=box, material=mkey)  # material_key=mkey
+    # TODO: materials_map?
+
+    scene_data_before = scene.data
+
+    usd_filename = os.path.join(DATA, "test_scene.usda")
+    gltf_filename = os.path.join(DATA, "test_scene.gltf")
+
+    scene.to_usd(usd_filename)
+    scene.to_gltf(gltf_filename)
+
+    scene = Scene.from_usd(usd_filename)
+    print("USD")
+    print("=======================")
     print(scene.data)
+
+    """
+
+    scene = Scene.from_gltf(gltf_filename)
+    print("GLTF")
+    print("=======================")
+    print(scene.data)
+
+    scene_data_after = scene.data
+
+    print(scene_data_before["node"])
+    print(scene_data_after["node"])
+
+    assert scene_data_before["materials"] == scene_data_after["materials"]
+    assert scene_data_before["attributes"] == scene_data_after["attributes"]
+    assert scene_data_before["dna"] == scene_data_after["dna"]
+    assert scene_data_before["dea"] == scene_data_after["dea"]
+    assert scene_data_before["adjacency"] == scene_data_after["adjacency"]
+    assert scene_data_before["edge"] == scene_data_after["edge"]
+    """
