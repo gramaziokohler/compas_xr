f9488440ddb9c5d8556ba093f2d7168036e63bcd
Romana Rust <rust@arch.ethz.ch>
2022-07-25 21:26:26 +0200

    Update material.py

--- src/compas_xr/datastructures/material.py
+++ src/compas_xr/datastructures/material.py
@@ -1,15 +1,69 @@
 from compas.data import Data
 
-# https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/schema/material.schema.json
+
+class AlphaMode(object):
+    """The alpha rendering mode of the material.
+
+    The material's alpha rendering mode enumeration specifying the interpretation of the alpha value of the base color.
+
+    Attributes
+    ----------
+    OPAQUE : str
+        The alpha value is ignored, and the rendered output is fully opaque.
+    MASK : str
+        The rendered output is either fully opaque or fully transparent depending on the alpha value and the specified `alphaCutoff` value; the exact appearance of the edges
+        **MAY** be subject to implementation-specific techniques such as \"`Alpha-to-Coverage`\".
+    BLEND : str
+        The alpha value is used to composite the source and destination areas. The rendered output is combined with the background using the normal painting operation (i.e. the
+        Porter and Duff over operator).
+    """
+
+    OPAQUE = "OPAQUE"
+    MASK = "MASK"
+    BLEND = "BLEND"
 
 
 class Material(Data):
-    """ """
+    """Class for managing a Material.
+
+    The class follows the schema for GLTF materials, as defined in
+    https://github.com/KhronosGroup/glTF/blob/main/specification/2.0/schema/material.schema.json
+
+
+    Attributes
+    ----------
+    name : str
+        The name of the material
+    pbr_metallic_roughness : :class:`compas_xr.datastructures.PBRMetallicRoughness`
+        A set of parameter values that are used to define the metallic-roughness material model from PBR methodology.
+    normal_texture : :class:`compas_xr.datastructures.NormalTexture`
+        The normal texture.
+    occlusion_texture : :class:`compas_xr.datastructures.OcclusionTexture`
+        The occlusion texture.
+    emissive_texture : :class:`compas_xr.datastructures.EmissiveTexture`
+        The emissive texture.
+    emissive_factor : list of 3 float
+        The factors for the emissive color of the material.
+    alpha_mode : :class:`compas_xr.datastructures.AlphaMode`
+        The alpha rendering mode of the material.
+    alpha_cutoff : float
+        The alpha cutoff value of the material.
+    double_sided : bool
+        Specifies whether the material is double sided.
+
+
+    Examples
+    --------
+    >>> material = Material(name="material")
+    >>> material.pbr_metallic_roughness = PBRMetallicRoughness()
+    >>> material.pbr_metallic_roughness.base_color_factor = [0.9, 0.4, 0.2, 1.0]
+    >>> material.pbr_metallic_roughness.metallic_factor = 0.0
+    >>> material.pbr_metallic_roughness.roughness_factor = 0.5
+    """
 
     def __init__(
         self,
         name=None,
-        extras=None,
         pbr_metallic_roughness=None,
         normal_texture=None,
         occlusion_texture=None,
@@ -20,21 +74,19 @@ class Material(Data):
         double_sided=True,
     ):
         self.name = name
-        self.extras = extras
         self.pbr_metallic_roughness = pbr_metallic_roughness
         self.normal_texture = normal_texture
         self.occlusion_texture = occlusion_texture
         self.emissive_texture = emissive_texture
-        self.emissive_factor = emissive_factor
-        self.alpha_mode = alpha_mode
-        self.alpha_cutoff = alpha_cutoff
-        self.double_sided = double_sided
+        self.emissive_factor = emissive_factor if emissive_factor else [0.0, 0.0, 0.0]
+        self.alpha_mode = alpha_mode if alpha_mode is not None else AlphaMode.OPAQUE
+        self.alpha_cutoff = alpha_cutoff if alpha_cutoff is not None else 0.5
+        self.double_sided = double_sided if double_sided is not None else True
 
     @property
     def data(self):
         return {
             "name": self.name,
-            "extras": self.extras,
             "pbr_metallic_roughness": self.pbr_metallic_roughness.data if self.pbr_metallic_roughness else None,  # noqa E501
             "normal_texture": self.normal_texture.data if self.normal_texture else None,  # noqa E501
             "occlusion_texture": self.occlusion_texture.data if self.occlusion_texture else None,  # noqa E501
@@ -48,7 +100,6 @@ class Material(Data):
     @data.setter
     def data(self, data):
         self.name = data.get("name")
-        self.extras = data.get("extras")
         self.pbr_metallic_roughness = PBRMetallicRoughness.from_data(data.get("pbr_metallic_roughness")) if data.get("pbr_metallic_roughness") else None  # noqa E501
         self.normal_texture = NormalTexture.from_data(data.get("normal_texture")) if data.get("normal_texture") else None  # noqa E501
         self.occlusion_texture = OcclusionTexture.from_data(data.get("occlusion_texture")) if data.get("occlusion_texture") else None  # noqa E501
@@ -133,6 +184,13 @@ class Texture(Data):
 
 
 class NormalTexture(Texture):
+    """The tangent space normal texture.
+
+    The texture encodes RGB components with linear transfer function. Each texel represents the XYZ components of a normal vector in tangent space. The normal vectors use the
+    convention +X is right and +Y is up. +Z points toward the viewer. If a fourth component (A) is present, it **MUST** be ignored. When undefined, the material does not have a
+    tangent space normal texture.
+    """
+
     def __init__(self, source=None, name=None, scale=None):
         super(NormalTexture, self).__init__(source, name)
         self.scale = scale
@@ -152,6 +210,12 @@ class NormalTexture(Texture):
 
 
 class OcclusionTexture(Texture):
+    """The occlusion texture.
+
+    The occlusion values are linearly sampled from the R channel. Higher values indicate areas that receive full indirect lighting and lower values indicate no indirect lighting.
+    If other channels are present (GBA), they **MUST** be ignored for occlusion calculations. When undefined, the material does not have an occlusion texture.
+    """
+
     def __init__(self, source=None, name=None, strength=None):
         super(OcclusionTexture, self).__init__(source, name)
         self.strength = strength
