f2d44834b21a74aab8ab30a851365ca9e285c7ad
Romana Rust <romana.rust@gmail.com>
2022-10-26 19:03:59 +0200

    Update __init__.py

--- src/compas_xr/__init__.py
+++ src/compas_xr/__init__.py
@@ -1,32 +1,42 @@
 """
 
-Intro to project ...
+COMPAS XR facilitates XR workflows to foster the application of XR technologies both in research and teaching. 
+It provides interfaces to existing state-of-the-art libraries and software platforms in the XR field (such as NVIDIA Omniverse™), 
+and makes them accessible from within the parametric CAD environment. 
 
-
-Setup
-=====
-
-In order to use this library, ...
+Compas XR tackles two core problems: data conversion (e.g USD, GLB support) and (-soon to come-) interaction. 
+The package builds upon COMPAS, an open-source Python-based framework for collaboration and research in architecture, 
+engineering and digital fabrication.
 
 
 Main concepts
 =============
 
-Describe typical classes found in project
+Scene
+-----
 
-.. autoclass:: SampleClassName
+.. autoclass:: Scene
    :members:
 
+
+Material
+-----
+
+.. autoclass:: Material
+   :members:
 
 """
 import os
 
-DATA = os.path.join(os.path.dirname(__file__), "data")
+from .__version__ import __version__
+from .datastructures import Scene
+from .datastructures import Material
 
-IN_OMNI = False
-try:
-    import omni  # noqa F401
+HERE = os.path.dirname(__file__)
+DATA = os.path.join(HERE, "data")
 
-    IN_OMNI = True
-except:  # ModuleNotFoundError: # noqa E722
-    IN_OMNI = False
+__all__ = [
+    "__version__",
+    "Scene",
+    "Material",
+]
