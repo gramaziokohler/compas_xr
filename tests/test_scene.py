e581be4f9d9d98b78d05c5e9fd3f87b12edc9850
Romana Rust <romana.rust@gmail.com>
2022-10-26 12:36:58 +0200

    Update test_scene.py

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -43,19 +43,18 @@ def scene_with_material_texture():
     image_uri = "compas_icon_white.png"
     image_file = os.path.join(BASE_FOLDER, "fixtures", image_uri)
     image_data = Image(name=image_uri, mime_type=MineType.PNG, uri=image_file)
-    image_idx = scene.add_image(image_data)
-    texture = Texture(source=image_idx)
-    texture_idx = scene.add_texture(texture)
+    # image_idx = scene.add_image(image_data)
+    texture = Texture(source=image_data)
+    # texture_idx = scene.add_texture(texture)
 
-    texture = Texture(source=image_idx)
-    texture_idx2 = scene.add_texture(texture)
+    # texture = Texture(source=texture)
+    # texture_idx2 = scene.add_texture(texture)
 
     material = Material()
     material.name = "Texture"
     material.pbr_metallic_roughness = PBRMetallicRoughness()
     material.pbr_metallic_roughness.metallic_factor = 0.0
-    material.pbr_metallic_roughness.base_color_texture = TextureInfo(index=texture_idx)
-    material_key = scene.add_material(material)
+    material.pbr_metallic_roughness.base_color_texture = TextureInfo(texture=texture)
 
     # add extension
     material.pbr_specular_glossiness = PBRSpecularGlossiness()
@@ -63,8 +62,10 @@ def scene_with_material_texture():
     material.pbr_specular_glossiness.specular_factor = [0.0, 0.0, 0.0]
     material.pbr_specular_glossiness.glossiness_factor = 0.0
     texture_transform = TextureTransform(rotation=0.0, scale=[2.0, 2.0])
-    material.pbr_specular_glossiness.diffuse_texture = TextureInfo(texture_idx2)
+    material.pbr_specular_glossiness.diffuse_texture = TextureInfo(texture=texture)
     material.pbr_specular_glossiness.diffuse_texture.texture_transform = texture_transform
+
+    material_key = scene.add_material(material)  # TODO
 
     # add box
     box = Box(Frame.worldXY(), 1, 1, 1)
@@ -104,8 +105,8 @@ def test_usd_export_import(scene_with_material):
     scene = Scene.from_usd(usd_filename)
     scene_data_after = scene.data
 
-    # assert scene_data_before["materials"] == scene_data_after["materials"]
-    # assert scene_data_before["attributes"] == scene_data_after["attributes"]
+    # assert scene_data_before["materials"] == scene_data_after["materials"] # TODO
+    # assert scene_data_before["attributes"] == scene_data_after["attributes"] # TODO {'name': 'scene'} != {'name': 'usd_scene'}
     assert scene_data_before["dna"] == scene_data_after["dna"]
     assert scene_data_before["dea"] == scene_data_after["dea"]
     assert scene_data_before["adjacency"] == scene_data_after["adjacency"]
