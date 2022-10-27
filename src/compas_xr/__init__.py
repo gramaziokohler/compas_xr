dc712cd5fbf2781ec100e232b48b3dcac81b85c2
Romana Rust <romana.rust@gmail.com>
2022-10-27 06:56:51 +0200

    Update __init__.py

--- src/compas_xr/__init__.py
+++ src/compas_xr/__init__.py
@@ -1,11 +1,10 @@
 """
+COMPAS XR facilitates XR workflows to foster the application of XR technologies both in research and teaching.
+It provides interfaces to existing state-of-the-art libraries and software platforms in the XR field (such as NVIDIA Omniverse),
+and makes them accessible from within the parametric CAD environment.
 
-COMPAS XR facilitates XR workflows to foster the application of XR technologies both in research and teaching. 
-It provides interfaces to existing state-of-the-art libraries and software platforms in the XR field (such as NVIDIA Omniverse™), 
-and makes them accessible from within the parametric CAD environment. 
-
-Compas XR tackles two core problems: data conversion (e.g USD, GLB support) and (-soon to come-) interaction. 
-The package builds upon COMPAS, an open-source Python-based framework for collaboration and research in architecture, 
+Compas XR tackles two core problems: data conversion (e.g USD, GLB support) and (-soon to come-) interaction.
+The package builds upon COMPAS, an open-source Python-based framework for collaboration and research in architecture,
 engineering and digital fabrication.
 
 
