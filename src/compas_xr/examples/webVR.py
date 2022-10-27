2fb7fc9b234d20c1cee111dc47c30286dbecd864
Romana Rust <romana.rust@gmail.com>
2022-10-27 06:56:28 +0200

    update examples

--- src/compas_xr/examples/webVR.py
+++
@@ -1,6 +0,0 @@
-from compas_xr.examples import upload_to_idl_vr
-
-if __name__ == "__main__":
-
-    gltf_filepath = r"C:\Users\rustr\workspace\compas_xr\src\compas_xr\data\mesh_with_material.gltf"
-    upload_to_idl_vr(gltf_filepath, htmlpage="webVR_001.html")
