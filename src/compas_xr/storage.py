3372a01013bb272391117ec25719bf182b7b3f7a
Joseph Kenny <jckenny59@gmail.com>
2023-10-09 18:42:15 +0200

    Update storage.py

--- src/compas_xr/storage.py
+++ src/compas_xr/storage.py
@@ -1,15 +1,29 @@
 import pyrebase
 import json
+import os
 
-config_path = "scripts\firebase_config.json"
+# Get the current file path
+current_file_path = os.path.abspath(__file__)
+print ("CURRENT FILE PATH = " + current_file_path)
 
-with open(config_path) as config_file:
-    config = json.load(config_file)
+# Define the number of levels to navigate up
+levels_to_go_up = 3
 
-    
-path_on_cloud = "test_assembly_withoutgeometry.json"  # path to download/upload from/to cloud
-# localpath to store files
-path_local = r"C:\Users\eleni\Documents\GitHub\cdf_unity\data\test_assembly_withoutgeometry.json"
+#Construct File path to the correct location
+parent_folder = os.path.abspath(os.path.join(current_file_path, "../" * levels_to_go_up))
+
+# Enter another folder
+target_folder = os.path.join(parent_folder, "scripts")
+print ("TARGET FOLDER PATH = " + target_folder)
+
+#JSON file path
+config_path = os.path.join(target_folder, "firebase_config.json")
+
+# Load the firebase configuration file from the JSON file if it exists: Else: Print.
+if os.path.exists(config_path):
+    with open(config_path) as config_file:
+        print (config_path)
+        config = json.load(config_file)
 
 # initialize the connection to firebase
 firebase = pyrebase.initialize_app(config)
@@ -27,8 +41,8 @@ def download_from_firebase(path_on_cloud, path_local):
     storage.child(path_on_cloud).download(path_local, filename)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    # download_from_firebase(path_on_cloud, path_local)
-    print(path_local)
-    upload_to_firebase(path_on_cloud, path_local)
+#     # download_from_firebase(path_on_cloud, path_local)
+#     print(path_local)
+#     upload_to_firebase(path_on_cloud, path_local)
