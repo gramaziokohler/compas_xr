89a33b33fb21d12526d7da39ffa13d550f50ccd1
Joseph Kenny <jckenny59@gmail.com>
2024-04-23 13:44:57 +0200

    realtimeDatabase_cli.py docstring formatting. Additionally restructuring _ensure_database()
    realtimeDatabase_cli.py docstring formatting. Additionally restructuring _ensure_database(): Mostly cosmetic, but additionally restructuring of ensure_database() method to throw exception first if the path does not exist.

--- src/compas_xr/realtime_database/realtime_database_cli.py
+++ src/compas_xr/realtime_database/realtime_database_cli.py
@@ -28,77 +28,101 @@ from Firebase.Auth import FirebaseAuthClient
 from Firebase.Database import FirebaseClient
 from Firebase.Database.Query import QueryExtensions
 
-"""
-TODO: add proper comments.
-TODO: Review Function todo's
-TODO: Authorization for the Database
-"""
-
 class RealtimeDatabase(RealtimeDatabaseInterface):
+    """
+    A RealtimeDatabase is defined by a Firebase configuration path and a shared database reference.
 
-    # Class attribute for the shared firebase database reference
+    The RealtimeDatabase class is responsible for initializing and managing the connection to a Firebase Realtime Database.
+    It ensures that the database connection is established only once and shared across all instances of the class.
+
+    Parameters
+    ----------
+    config_path : str
+        The path to the Firebase configuration JSON file.
+
+    Attributes
+    ----------
+    config_path : str
+        The path to the Firebase configuration JSON file.
+    database : FirebaseClient
+        The FirebaseClient instance representing the connection to the Firebase Realtime Database.
+    _shared_database : FirebaseClient, class attribute
+        The shared FirebaseClient instance representing the connection to the Firebase Realtime Database.
+
+    Methods
+    -------
+    _ensure_database() : FirebaseClient
+        Ensures that the database connection is established. If the connection is not yet established, it initializes it. If the connection is already established, it returns the existing connection.
+
+    Raises
+    ------
+    Exception
+        If the configuration file does not exist at the provided path or if the database could not be initialized.
+
+    Examples
+    --------
+    >>> db = RealtimeDatabase('path/to/config.json')
+    >>> db.config_path
+    """
     _shared_database = None
-    
-    def __init__(self, config_path = None):
-        
-        self.config_path = config_path #or DEFAULT_CONFIG_PATH
+
+    def __init__(self, config_path):
+        self.config_path = config_path
         self.database = self._ensure_database()
 
-    #Internal Class Structure Functions
     def _ensure_database(self):
-
-        # Initialize Firebase connection and databse only once
+        """
+        Ensures that the database connection is established.
+        If the connection is not yet established, it initializes it.
+        If the connection is already established, it returns the existing connection.
+        """
         if not RealtimeDatabase._shared_database:
             path = self.config_path
+            if not os.path.exists(path):
+                raise Exception("Could not find config file at path {}!".format(path))
+            with open(path) as config_file:
+                config = json.load(config_file)
+            #TODO: Database Authorization (Works only with public databases)
+            database_url = config["databaseURL"]
+            database_client = FirebaseClient(database_url)
+            RealtimeDatabase._shared_database = database_client
 
-            # Load the Firebase configuration file from the JSON file if the file exists
-            if os.path.exists(path):
-                with open(path) as config_file:
-                    config = json.load(config_file)
-
-                #TODO: Authorization for database security (Works for now for us because our DB is public)
-
-                #Initilize database instance from the database URL
-                database_url = config["databaseURL"]
-                database_client = FirebaseClient(database_url)
-                RealtimeDatabase._shared_database = database_client
-                print ("Shared Database Client Set")
-        
-        # Still no Database? Fail, we can't do anything
         if not RealtimeDatabase._shared_database:
             raise Exception("Could not initialize Database!")
 
         return RealtimeDatabase._shared_database
 
     def _start_async_call(self, fn, timeout=10):
-        
+        """
+        Manages asynchronous calls to the database.
+        """
         result = {}
         result["event"] = threading.Event()
-        
         async_thread = threading.Thread(target=fn, args=(result, ))
         async_thread.start()
         async_thread.join(timeout=timeout)
-
         return result["data"]
 
-    def _get_file_from_remote(self, url):
-        
+    def _get_file_from_remote(self, url):        
         """
         This function is used to get the information form the source url and returns a string
         It also checks if the data is None or == null (firebase return if no data)
         """
-
         try:
             get = urlopen(url).read()
-
         except:
             raise Exception("unable to get file from url {}".format(url))
-        
         if get is not None and get != "null":
             return get    
-        
         else:
             raise Exception("unable to get file from url {}".format(url))
+
+    #TODO: Can this be configured to be a global callback for all methods where I pass the task and the result? This would simplify the code a lot.
+    def _task_callback(self, task, result):
+        task_awaiter = task.GetAwaiter()
+        task_awaiter.OnCompleted(lambda: result["event"].set())
+        result["event"].wait()
+        result["data"] = True
 
     def construct_reference(self, parentname):
         """
@@ -187,15 +211,7 @@ class RealtimeDatabase(RealtimeDatabaseInterface):
                 reference = reference.Child(ref)
             else:
                 reference = QueryExtensions.Child(reference, ref)
-            print (dir(reference))
         return reference
-
-    #TODO: Can this be configured to be a global callback for all methods where I pass the task and the result? This would simplify the code a lot.
-    def _task_callback(self, task, result):
-        task_awaiter = task.GetAwaiter()
-        task_awaiter.OnCompleted(lambda: result["event"].set())
-        result["event"].wait()
-        result["data"] = True
 
     def upload_data_to_reference(self, data, database_reference):
         """
@@ -212,24 +228,19 @@ class RealtimeDatabase(RealtimeDatabaseInterface):
         -------
         None
         """
-        print (type(data))
-        print (type(database_reference))
         self._ensure_database()
         serialized_data = json_dumps(data)
-
         def _begin_upload(result):
             uploadtask = database_reference.PutAsync(serialized_data)
             task_upload = uploadtask.GetAwaiter()
-            print (dir(task_upload))
             task_upload.OnCompleted(lambda: result["event"].set())
             result["event"].wait()
             result["data"] = True
-
         self._start_async_call(_begin_upload)
 
     def get_data_from_reference(self, database_reference):
         """
-        Method for reteriving data from a constructed database reference.
+        Method for retrieving data from a constructed database reference.
 
         Parameters
         ----------
@@ -238,21 +249,19 @@ class RealtimeDatabase(RealtimeDatabaseInterface):
 
         Returns
         -------
-        None
+        dict
+            The retrieved data as a dictionary.
+
         """
         self._ensure_database()
-
         def _begin_build_url(result):
             urlbuldtask = database_reference.BuildUrlAsync()
             task_url = urlbuldtask.GetAwaiter()
             task_url.OnCompleted(lambda: result["event"].set())
             result["event"].wait()
             result["data"] = urlbuldtask.Result
-        
         url = self._start_async_call(_begin_build_url)
-
-        data = self._get_file_from_remote(url)
-        print (type(data))
+        json_data = self._get_file_from_remote(url)
 
         #TODO: json.load(data) vs. json_loads(data)
         """
@@ -261,23 +270,31 @@ class RealtimeDatabase(RealtimeDatabaseInterface):
         Therefore, unless the data is completely filled json_loads(data) will throw an error.
 
         """
-        dictionary = json.loads(data)
-        print (type(dictionary))
-  
-        return dictionary
+        data = json.loads(json_data)
+        return data
 
     def delete_data_from_reference(self, database_reference):
+        """
+        Method for uploading data to a constructed database reference.
 
-        #Ensure Database Connection
+        Parameters
+        ----------
+        data : Any
+            The data to be uploaded. Data should be JSON serializable.
+        database_reference: 'Firebase.Database.Query.ChildQuery'
+            Reference to the database location where the data will be uploaded.
+
+        Returns
+        -------
+        None
+        """
         self._ensure_database()
-
         def _begin_delete(result):
             deletetask = database_reference.DeleteAsync()
             delete_data = deletetask.GetAwaiter()
             delete_data.OnCompleted(lambda: result["event"].set())
             result["event"].wait()
             result["data"] = True
-        
         self._start_async_call(_begin_delete)
 
     def stream_data_from_reference(self, callback, database_reference):
