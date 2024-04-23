1a1a3cd71b6daa05a488e21141661fb7278b2d62
Joseph Kenny <jckenny59@gmail.com>
2024-04-23 17:17:23 +0200

    update realtime_database_pyrebase.py upload_data_to_reference()
    This one I am having a very difficult time with. I was hoping that I could treat it the same as in the _cli..... where I pass it data, serialize the data (with json_dumps(data)), and then upload. However, all of the pushing methods I can find from pyrebase do the json serilization logic within the method. This is causing differences in the methods across both child classes. details in comment.

--- src/compas_xr/realtime_database/realtime_database_pyrebase.py
+++ src/compas_xr/realtime_database/realtime_database_pyrebase.py
@@ -9,31 +9,64 @@ import pyrebase
 from compas.data import json_dumps,json_loads
 
 class RealtimeDatabase(RealtimeDatabaseInterface):
+    """
+    A RealtimeDatabase is defined by a Firebase configuration path and a shared database reference.
 
-    # Class attribute for the shared firebase database reference
+    The RealtimeDatabase class is responsible for initializing and managing the connection to a Firebase Realtime Database.
+    It ensures that the database connection is established only once and shared across all instances of the class.
+
+    Parameters
+    ----------
+    config_path : str
+        The path to the Firebase configuration JSON file.
+
+    Attributes
+    ----------
+    config_path : str
+        The path to the Firebase configuration JSON file.
+    database : Database
+        The Database instance representing the connection to the Firebase Realtime Database.
+    _shared_database : Database, class attribute
+        The shared Database instance representing the connection to the Firebase Realtime Database.
+
+    Methods
+    -------
+    _ensure_database() : Database
+        Ensures that the database connection is established. If the connection is not yet established, it initializes it. If the connection is already established, it returns the existing connection.
+
+    Raises
+    ------
+    Exception
+        If the configuration file does not exist at the provided path or if the database could not be initialized.
+
+    Examples
+    --------
+    >>> db = RealtimeDatabase('path/to/config.json')
+    >>> db.config_path
+    """
     _shared_database = None
 
-    def __init__(self, config_path=None):
+    def __init__(self, config_path):
         self.config_path = config_path
-        self._guid = None
         self._ensure_database()
 
     def _ensure_database(self):
-        # Initialize firebase connection and storage only once
+        """
+        Ensures that the database connection is established.
+        If the connection is not yet established, it initializes it.
+        If the connection is already established, it returns the existing connection.
+        """
         if not RealtimeDatabase._shared_database:
             path = self.config_path
             
-            # Load the firebase configuration file from the JSON file if it exists: Else: Print.
-            if os.path.exists(path):
-                with open(path) as config_file:
-                    config = json.load(config_file)
-
-            #TODO: Authorization for Protected Databases
-
+            if not os.path.exists(path):
+                raise Exception("Could not find config file at path {}!".format(path))
+            with open(path) as config_file:
+                config = json.load(config_file)
+            #TODO: Database Authorization (Works only with public databases)
             firebase = pyrebase.initialize_app(config)
             RealtimeDatabase._shared_database = firebase.database()
 
-        # Still no Database? Fail, we can't do anything
         if not RealtimeDatabase._shared_database:
             raise Exception("Could not initialize database!")
 
@@ -151,208 +184,25 @@ class RealtimeDatabase(RealtimeDatabaseInterface):
         data_dict = dict(data)
         return data_dict
 
-    #Functions for uploading .json files specifically
-    def upload_file_all(self, path_local, parentname): 
-        
-        self._ensure_database()
-        
-        if os.path.exists(path_local):
-            with open(path_local) as json_file:
-                json_data = json.load(json_file)
-        
-        else:
-            raise Exception("path does not exist {}".format(path_local))
-        
-        RealtimeDatabase._shared_database.child(parentname).set(json_data)
-        print ("upload complete")
-
-    def upload_file(self, path_local, parentname, parentparameter, parameters): 
-        
-        self._ensure_database()
-        
-        if os.path.exists(path_local):
-            with open(path_local) as json_file:
-                json_data = json.load(json_file)
-                children = []
-                
-                for param in parameters:
-                    values = json_data[parentparameter][param]
-                    children.append(values)
-
-        else:
-            raise Exception("path does not exist {}".format(path_local))
-
-        for child, param in zip(children, parameters):
-            RealtimeDatabase._shared_database.child(parentname).child(param).set(child)
-        print ("upload complete")
-
-    def upload_file_baselevel(self, path_local, parentname, parameters): 
-        
-        self._ensure_database()
-        
-        if os.path.exists(path_local):
-            with open(path_local) as json_file:
-                json_data = json.load(json_file)
-                children = []
-                
-                for param in parameters:
-                    values = json_data[param]
-                    children.append(values)
-
-        else:
-            raise Exception("path does not exist {}".format(path_local))
-
-        for child, param in zip(children, parameters):
-            RealtimeDatabase._shared_database.child(parentname).child(param).set(child)
-        print ("file uploaded")
-
-    #Functions for uploading Data
-    def upload_data_all(self, data, parentname): 
-        
-        self._ensure_database() 
-
-        #Serialize data
-        serialized_data = json_dumps(data)
-        
-        RealtimeDatabase._shared_database.child(parentname).set(serialized_data)
-        print ("data uploaded")
-
-    def upload_data(self, data, parentname,  parentparamater, parameters): 
-        #Check Database Reference            
-        self._ensure_database() 
-        
-        #Serialize data
-        serialized_data = json_dumps(data)
-
-        children = []
-
-        for param in parameters:
-            values = serialized_data[parentparamater][param]
-            children.append(values)
-
-        for child, param in zip(children, parameters):
-            RealtimeDatabase._shared_database.child(parentname).child(param).set(child)
-        print ("upload complete")
-
-    #Functions for uploading and nesting in the database
-    def upload_file_aschild(self, path_local, parentname, childname, parentparameter, childparameter): 
-        
-        self._ensure_database()
-        
-        if os.path.exists(path_local):
-            with open(path_local) as json_file:
-                json_data = json.load(json_file)
-
-        else:
-            raise Exception("path does not exist {}".format(path_local))
-
-        #TODO: Check if Serialization is needed.
-        values = json_data[parentparameter][childparameter]
-        
-        RealtimeDatabase._shared_database.child(parentname).child(childname).set(values)
-        print ("upload complete")
-
-    def upload_file_aschildren(self, path_local, parentname, childname, parentparameter, childparameter, parameters):
-        
-        self._ensure_database()
-        
-        if os.path.exists(path_local):
-            with open(path_local) as json_file:
-                json_data = json.load(json_file)
-
-        else:
-            raise Exception("path does not exist {}".format(path_local))
-
-        children = []
-        
-        for param in parameters:
-            values = json_data[parentparameter][childparameter][param]
-            children.append(values)
-
-        for child, param in zip(children, parameters):
-            RealtimeDatabase._shared_database.child(parentname).child(childname).child(param).set(child)
-        print ("upload complete")
-    
-    #TODO: CHANGE NAME
-    def upload_data_aschild(self, data, parentname, childname):
-           
-        #Ensure Database Connection
-        self._ensure_database()
-        
-        serialized_data = json_dumps(data)
-        
-        RealtimeDatabase._shared_database.child(parentname).child(childname).set(serialized_data)
-        print("upload complete")
-
-    #TODO: CHANGE NAME
-    def upload_data_aschildren(self, data, parentname, childname, name):
-            
-        #Ensure Database Connection
-        self._ensure_database()
-
-        serialized_data = json_dumps(data)
-
-        RealtimeDatabase._shared_database.child(parentname).child(childname).child(name).set(serialized_data)
-        print ("upload complete")
-
-    #Functions for retreiving infomation from the database Streaming and Downloading
-    def stream_parent(self, callback, parentname): #TODO: Needs to be fixed
+    def stream_data_from_reference(self, callback, database_reference):
         raise NotImplementedError("Function Under Developement")
-    
-    def get_parent(self, parentname): 
-        self._ensure_database()
-        dictionary = {}
-        database_reference = RealtimeDatabase._shared_database.child(parentname).get()
-        temp_data = database_reference.val()
-        print("type of temp_data", type(temp_data))
-        print("temp_data", temp_data)
-        test_dict = dict(temp_data)
-        print("type of test_dict", type(test_dict))
-        print("test_dict", test_dict)
-        # json_load_data = json.load(temp_data)
-        # print("type of json_load_data", type(json_load_data))
-        # print("json_load_data", json_load_data)
 
-        if database_reference.each():
-            for d in database_reference.each():
-                dictionary[d.key()] = d.val()
-            print ("got parent")
-            print("dictionary", dictionary)
-            print("type of dictionary", type(dictionary))
-            return dictionary
-        else:
-            raise Exception("Parent not Found in database")
+    def upload_data_to_reference(self, data, database_reference): 
+        """
+        Method for uploading data to a constructed database reference.
 
-    def get_child(self, parentname, childname): 
-        self._ensure_database()
-        
-        dictionary = {}
-        database_reference = RealtimeDatabase._shared_database.child(parentname).child(childname).get()
-        
-        if database_reference.each():
-            for d in database_reference.each():
-                dictionary[d.key()] = d.val()
-            print ("got child")
-            return dictionary
-        
-        else:
-            raise Exception("Child not Found in database")
-    
-    #Functions for deleting parents and children
-    def delete_parent(self, parentname):
-        self._ensure_database()
-        RealtimeDatabase._shared_database.child(parentname).remove()
-        print ("parent deleted")
+        Parameters
+        ----------
+        data : Any
+            The data to be uploaded. Data should be JSON serializable.
+        database_reference: 'pyrebase.pyrebase.Database'
+            Reference to the database location where the data will be uploaded.
 
-    def delete_child(self, parentname, childname):
+        Returns
+        -------
+        None
+        """
         self._ensure_database()
-        RealtimeDatabase._shared_database.child(parentname).child(childname).remove()
-        print ("child deleted")
-
-    def delete_children(self, parentname, childname, children):
-        self._ensure_database()
-
-        for child in children:
-            RealtimeDatabase._shared_database.child(parentname).child(childname).child(child).remove()
-        print ("children deleted")
-  
+        #TODO: Check if it is possible to do this with some sort of serialization for consistency across both?
+        # serialized_data = json_dumps(data)
+        database_reference.set(data)  
