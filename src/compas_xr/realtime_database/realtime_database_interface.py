1b9710901068557fe5e708dcf1847f53de43bcb4
Joseph Kenny <jckenny59@gmail.com>
2024-04-23 18:44:51 +0200

    add RealtimeDatabaseInterface delete_data_from_deep_reference()
    method allows for deleting information from very nested data structures.

--- src/compas_xr/realtime_database/realtime_database_interface.py
+++ src/compas_xr/realtime_database/realtime_database_interface.py
@@ -27,91 +27,50 @@ class RealtimeDatabaseInterface(object):
     def stream_data_from_reference(self, callback, database_reference):
         raise NotImplementedError("Implemented on child classes")
 
+    def application_settings_writer(self, databaseparentname, storagefolder="None", objorientation=True):
+        data = {"parentname": databaseparentname, "storagename": storagefolder, "objorientation": objorientation}
+        self.upload_data(data, "ApplicationSettings")
+
     def upload_data(self, data, project_name):
         database_reference = self.construct_reference(project_name)
         self.upload_data_to_reference(data, database_reference)
-
-    def upload_data_from_file(self, path_local, project_name):
-        if os.path.exists(path_local):
-            with open(path_local) as config_file:
-                data = json.load(config_file)
-            database_reference = self.construct_reference(project_name)
-            self.upload_data_to_reference(data, database_reference)
-        else:
-            raise Exception("path does not exist {}".format(path_local))
-
-    def upload_data_to_deep_reference(self, data, reference_list):
-        database_reference = self.construct_reference_from_list(reference_list)
-        self.upload_data_to_reference(data, database_reference)
-    
-    def get_data_from_deep_reference(self, reference_list):
-        database_reference = self.construct_reference_from_list(reference_list)
-        return self.get_data_from_reference(database_reference)
-
-    def get_data(self, project_name):
-        database_reference = self.construct_reference(project_name)
-        return self.get_data_from_reference(database_reference)
 
     def upload_data_to_project(self, data, project_name, child_name):
         database_reference = self.construct_child_refrence(project_name, child_name)
         self.upload_data_to_reference(data, database_reference)
 
+    def upload_data_to_deep_reference(self, data, reference_list):
+        database_reference = self.construct_reference_from_list(reference_list)
+        self.upload_data_to_reference(data, database_reference)
+
+    def upload_data_from_file(self, path_local, project_name):
+        if not os.path.exists(path_local):
+            raise Exception("path does not exist {}".format(path_local))
+        with open(path_local) as config_file:
+            data = json.load(config_file)
+        database_reference = self.construct_reference(project_name)
+        self.upload_data_to_reference(data, database_reference)
+
+    def get_data(self, project_name):
+        database_reference = self.construct_reference(project_name)
+        return self.get_data_from_reference(database_reference)
+
     def get_data_from_project(self, project_name, child_name):
         database_reference = self.construct_child_refrence(project_name, child_name)
+        return self.get_data_from_reference(database_reference)
+
+    def get_data_from_deep_reference(self, reference_list):
+        database_reference = self.construct_reference_from_list(reference_list)
         return self.get_data_from_reference(database_reference)
     
     def delete_data(self, project_name):
         database_reference = self.construct_reference(project_name)
         self.delete_data_from_reference(database_reference)
-    
+
     def delete_data_from_project(self, project_name, child_name):
         database_reference = self.construct_child_refrence(project_name, child_name)
         self.delete_data_from_reference(database_reference)
 
-    def upload_file_all(self, path_local, parentname):
-        raise NotImplementedError("Implemented on child classes")
-
-    def upload_file(self, path_local, parentname, parentparameter, parameters): 
-        raise NotImplementedError("Implemented on child classes")
-
-    def upload_file_baselevel(self, path_local, parentname, parameters): 
-        raise NotImplementedError("Implemented on child classes")
-
-    def upload_data_all(self, data, parentname): 
-        raise NotImplementedError("Implemented on child classes")
-    
-    def upload_file_aschild(self, path_local, parentname, childname, parentparameter, childparameter):
-        raise NotImplementedError("Implemented on child classes")
-
-    def upload_file_aschildren(self, path_local, parentname, childname, parentparameter, childparameter, parameters):
-        raise NotImplementedError("Implemented on child classes")
-
-    def upload_data_aschild(self, data, parentname, childname, parentparameter, childparameter):
-        raise NotImplementedError("Implemented on child classes")
-
-    def upload_data_aschildren(self, data, parentname, childname, parentparameter, childparameter, parameters):
-        raise NotImplementedError("Implemented on child classes")
-    
-    def stream_parent(self, callback, parentname):
-        raise NotImplementedError("Implemented on child classes")
-
-    def download_parent(self, parentname): 
-        raise NotImplementedError("Implemented on child classes")
-
-    def download_child(self, parentname, childname): 
-        raise NotImplementedError("Implemented on child classes")
-
-    def delete_parent(self, parentname):
-        raise NotImplementedError("Implemented on child classes")
-
-    def delete_child(self, parentname, childname):
-        raise NotImplementedError("Implemented on child classes")
-
-    def delete_children(self, parentname, childname, children):
-        raise NotImplementedError("Implemented on child classes")
-    
-    def application_settings_writer(self, databaseparentname, storagefolder="None", objorientation=True):
-        
-        data = {"parentname": databaseparentname, "storagename": storagefolder, "objorientation": objorientation}
-
-        self.upload_data_all(data, "ApplicationSettings")
+    def delete_data_from_deep_reference(self, reference_list):
+        database_reference = self.construct_reference_from_list(reference_list)
+        self.delete_data_from_reference(database_reference)
