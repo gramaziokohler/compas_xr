5828a1d2341492c4dfc067a11acf6b880fec33d8
Joseph Kenny <jckenny59@gmail.com>
2024-04-24 11:10:42 +0200

    update AssemblyExtentsions
    - Docstrings
    - remove empty __init__
    - Mesh assembly not containing frame explanation
    - Throw Exception before path exports

--- src/compas_xr/assemblies/assembly_extensions.py
+++ src/compas_xr/assemblies/assembly_extensions.py
@@ -12,25 +12,35 @@ from compas_timber.consumers import BrepGeometryConsumer
 
 class AssemblyExtensions(object):
 
-    def __init__(self):
-        pass
-
     def export_timberassembly_objs(self, assembly, folder_path, new_folder_name, z_to_y_remap=False):
+        """
+        Export timber assembly beams as .obj files to a folder path.
 
-        #Construct file path with the new folder name
+        Parameters
+        ----------
+        assembly : compas_timber.assembly.TimberAssembly
+            The assembly that you want to export beams from.
+        folder_path : str
+            The path in which you would like to create a storage folder.
+        new_folder_name : str
+            The name of the folder you would like to create.
+        z_to_y_remap : bool, optional
+            A boolean that determines if the z-axis should be remapped to the y-axis for .obj export. Default is False.
+
+        Returns
+        -------
+        None
+
+        """
         target_folder_path = os.path.join(folder_path, new_folder_name)
-        
-        #Make a folder with the folder name if it does not exist
         if not os.path.exists(target_folder_path):
             os.makedirs(target_folder_path)
 
-        #Create frame options for orientation of objects
         if z_to_y_remap:
             frame = Frame(Point(0,0,0), Vector.Xaxis(), Vector.Zaxis())
         else:
             frame = Frame.worldXY()
-        
-        #Loop through the geometry from the Brep Consumer
+
         for result in BrepGeometryConsumer(assembly).result:
             brep = result.geometry
             beam_frame = result.beam.frame
@@ -40,50 +50,70 @@ class AssemblyExtensions(object):
             compas_mesh = Mesh()
             for mesh in brep_meshes:
                 compas_mesh.join(mesh)
-
-            compas_mesh.transform(Transformation.from_frame_to_frame(beam_frame, frame))
+            compas_mesh.transformed(Transformation.from_frame_to_frame(beam_frame, frame))
                         
-            # Mesh to obj with file name being KEY and folder being the newly created folder
-            if os.path.exists(target_folder_path):
-                compas_mesh.to_obj(r"{}\{}.obj".format(target_folder_path, str(key)))
-            
-            #Raise Exception if the target folder does not exist.
-            else:
-                raise Exception("File path does not exist {}".format(target_folder_path))
+            if not os.path.exists(target_folder_path):
+                raise Exception("File path does not exist {}".format(target_folder_path))                
+            filename = "{}.obj".format(str(key))
+            compas_mesh.to_obj(os.path.join(target_folder_path, filename))
 
     def export_mesh_assembly_objs(self, assembly, folder_path, new_folder_name, z_to_y_remap=False):
-        
-        #Construct file path with the new folder name
+        """
+        Export Mesh assembly parts as .obj files to a folder path.
+
+        Parameters
+        ----------
+        assembly : compas.datastructures.Assembly
+            The Mesh (compas.datastructures.Mesh) assembly that you want to export parts from.
+        folder_path : str
+            The path in which you would like to create a storage folder.
+        new_folder_name : str
+            The name of the folder you would like to create.
+        z_to_y_remap : bool, optional
+            A boolean that determines if the z-axis should be remapped to the y-axis for .obj export. Default is False.
+
+        Returns
+        -------
+        None
+
+        """
         target_folder_path = os.path.join(folder_path, new_folder_name)
-        
-        #Make a folder with the folder name if it does not exist
         if not os.path.exists(target_folder_path):
             os.makedirs(target_folder_path)
 
-        #Create frame options for orientation of objects
         if z_to_y_remap:
             frame = Frame(Point(0,0,0), Vector.Xaxis(), Vector.Zaxis())
         else:
             frame = Frame.worldXY()
 
-        #TODO: CONFIRM TRY EXCEPT BLOCK WITH GONZALO.
         for part in assembly.parts():
+            #Mesh assembly can be made with or without a frame (ex. assembly.add_part(Mesh)) try & default to worldXY
             try:
                 part_frame = part.frame
             except:
                 part_frame = Frame.worldXY()
-            
-            #Transform mesh part from frame to to frame
             part_transformed = part.transformed(Transformation.from_frame_to_frame(part_frame, frame))
-            
-            # Mesh to obj with file name being KEY and folder being the newly created folder
-            if os.path.exists(target_folder_path):
-                part_transformed.to_obj(r"{}\{}.obj".format(target_folder_path, str(part.key)))
-            
-            else:
+
+            if not os.path.exists(target_folder_path):
                 raise Exception("File path does not exist {}".format(target_folder_path))
+            filename = "{}.obj".format(str(part.key))
+            part_transformed.to_obj(os.path.join(target_folder_path, filename))                
 
     def create_qr_assembly(self, qr_frames):
+        """
+        Create a frame assembly from a list of compas.geometry.Frames with a specific data structure for localization data.
+
+        Parameters
+        ----------
+        qr_frames : list of 'compas.geometry.Frame'
+            A list of frames at specific locations for application localization data.
+
+        Returns
+        -------
+        :class: 'compas.datastructures.Assembly'
+            The constructed database reference.
+
+        """
         assembly = Assembly()
         for frame in qr_frames:
             part = Part(frame, frame=frame)
